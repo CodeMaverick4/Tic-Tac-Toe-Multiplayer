b2277ab1bf8e179c9b429cc3f840f24ea245a541
Burhan Ahmed <ahmed.burhan.ds@gmail.com>
2024-07-04 10:49:30 +0530

    resolve the error of express require

--- server/tic_tac_toe_server.js
+++ server/tic_tac_toe_server.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 const checkWin = require('./checking_win _condition')
 const path = require('path');
